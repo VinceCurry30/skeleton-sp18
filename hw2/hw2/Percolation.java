e577b04c009d212327f5c3b278a8e6205fe7a50d
申琳 <linshen@Lins-MacBook-Pro.local>
2020-02-16 18:13:15 -0600

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,131 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int size;
+    // the value in grid represent the site is open(true) or blocked(false)
+    private Boolean[][] grid;
+    private WeightedQuickUnionUF DS;
+    private WeightedQuickUnionUF DSwithvirtualtop;
+    private int opensitesnumber;
 
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        size = N;
+        grid = new Boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = false;
+            }
+        }
+        // last one is for virtual bottom site, second to last one is for virtual top site
+        DS = new WeightedQuickUnionUF(size * size + 2);
+        // avoid backwash
+        DSwithvirtualtop = new WeightedQuickUnionUF(size * size + 1);
+        opensitesnumber = 0;
+    }
+
+    private int xyTo1D(int r, int c) {
+        return r * size + c;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        grid[row][col] = true;
+        opensitesnumber += 1;
+        int number = xyTo1D(row, col);
+        if (row != 0) {
+            if (isOpen(row - 1, col)) {
+                DS.union(number, number - size);
+                DSwithvirtualtop.union(number, number - size);
+            }
+        }
+        if (row != size - 1) {
+            if (isOpen(row + 1, col)) {
+                DS.union(number, number + size);
+                DSwithvirtualtop.union(number, number + size);
+            }
+        }
+        if (col != 0) {
+            if (isOpen(row, col - 1)) {
+                DS.union(number, number - 1);
+                DSwithvirtualtop.union(number, number - 1);
+            }
+        }
+        if (col != size - 1) {
+            if (isOpen(row, col + 1)) {
+                DS.union(number, number + 1);
+                DSwithvirtualtop.union(number, number + 1);
+            }
+        }
+
+        // if open the site in the first row, union it with the virtual top site
+        if (row == 0) {
+            DS.union(number, size * size);
+            DSwithvirtualtop.union(number, size * size);
+        }
+        // if open the site in the last row, union it with the virtual bottom site
+        if (row == size - 1) {
+            DS.union(number, size * size + 1);
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return grid[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        int number = xyTo1D(row, col);
+        /*
+        for (int i = 0; i < size; i++) {
+            if (DS.connected(number, i) && isOpen(row, col)) {
+                return true;
+            }
+        }
+        return false;
+        */
+        if (DSwithvirtualtop.connected(number, size * size)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return opensitesnumber;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        /*
+        for (int i = 0; i < size; i++) {
+            if (isFull(size - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+        */
+        if (DS.connected(size * size, size * size + 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,47 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import java.lang.Math;
 
+public class PercolationStats {
+    private int count;
+    private Percolation[] perco;
+    private double[] openfraction;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        count = T;
+        perco = new Percolation[T];
+        for (Percolation p : perco) {
+            p = pf.make(N);
+        }
+        openfraction = new double[T];
+        for (int i = 0; i < T; i++) {
+            while (!perco[i].percolates()) {
+                perco[i].open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            openfraction[i] = perco[i].numberOfOpenSites() * 1.0 / (N * N * 1.0);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(openfraction);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(openfraction);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(count);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(count);
+    }
 }
