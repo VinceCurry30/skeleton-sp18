4dfc5cdc156e4b5e07e547e7217f73cabc891fe4
申琳 <linshen@Lins-MacBook-Pro.local>
2020-05-27 11:43:18 -0500

    finish cycle check part of lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,50 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] parent;
+    private boolean isCycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        parent = new int[m.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(0);
     }
 
     // Helper methods go here
-}
+    private void dfs(int v) {
+        if (isCycle) {
+            return;
+        }
 
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (marked[w] && parent[v] != w) {
+                parent[w] = v;
+                isCycle = true;
+                setEdgeTo(w);
+                announce();
+                break;
+            }
+            if (!marked[w]) {
+                parent[w] = v;
+                announce();
+                dfs(w);
+            }
+        }
+    }
+
+    private void setEdgeTo(int v) {
+        edgeTo[v] = parent[v];
+        int temp = parent[v];
+        while (temp != v) {
+            edgeTo[temp] = parent[temp];
+            temp = parent[temp];
+        }
+    }
+}
